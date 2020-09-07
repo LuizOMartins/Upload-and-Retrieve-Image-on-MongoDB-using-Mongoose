ddbae0bf9b122c0b3f8d4571d081d6e8f20f9bd2
Luiz Eduardo <43974104+LuizOMartins@users.noreply.github.com>
2020-09-07 10:04:31 -0300

    Erro para exibir imagem no Front-End AngularJS

--- public/js/controllers/image-controller.js
+++ public/js/controllers/image-controller.js
@@ -2,20 +2,22 @@ angular.module('image')
 	.controller('ImageController', function($scope, $http) {
 
 		$scope.imagens = {};
+		// $scope.data = {};
 		
 		$scope.getImages = function(){
 			console.log("CLICOU");
 			$http.get('/image')
-			.success(function(imagens) {
-				$scope.imagens = imagens;
-			console.log("IMAGENS",imagens);
+			.success(function(response) {
+				$scope.imagens = response;
+			console.log("IMAGENS",response);
+			// console.log(imagens.img);
 		})
 		.error(function(erro) {
 			console.log(erro);
 		});
 		}
 
-		
+
 
 
 	});
