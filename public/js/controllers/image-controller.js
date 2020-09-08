84441af8b89d033fd426d12435aa140b971cf23e
Luiz Eduardo <43974104+LuizOMartins@users.noreply.github.com>
2020-09-07 22:32:23 -0300

    Salvando Status

--- public/js/controllers/image-controller.js
+++ public/js/controllers/image-controller.js
@@ -6,21 +6,19 @@ angular.module('image')
 		// $scope.data = {};
 		
 		$scope.getImages = function(){
-			console.log("CLICOU");
 			$http.get('/image')
 			.success(function(response) {
 				$scope.imagens = response;
 
-				$scope.imagens.forEach(element => {
-					console.log('Nome:',element.name);
-					console.log('Image :',element.img);
-					console.log('Image DATA:',element.img.data);
-					console.log("TYPE",typeof element.img);
-					
-				});
+				// $scope.imagens.forEach(element => {
+				// 	console.log('Nome:',element.name);
+				// 	console.log('Image :',element.img);
+				// 	console.log('Image DATA:',element.img.data);
+				// 	console.log("TYPE",typeof element.img.data.data);
+				// 	// element.img.data = element.img.data.toString('base64');
+				// });
 
 			console.log("IMAGENS",response);
-			// console.log(imagens.img);
 		})
 		.error(function(erro) {
 			console.log(erro);
